89fc88d92adbc7c342dedeeee63ab96599490f35
Aulia Atha <auliaatha123@gmail.com>
2025-06-03 21:13:57 +0700

    Sorting Middleware

--- laravel 12/the_basic/bootstrap/app.php
+++ laravel 12/the_basic/bootstrap/app.php
@@ -9,11 +9,11 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    
+
     ->withMiddleware(function (Middleware $middleware) {
 
         // Global Middleware
@@ -29,6 +29,13 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias(['token', EnsureTokenIsValid::class]);
         $middleware->alias(['first', First::class]);
         $middleware->alias(['second', Second::class]);
+
+        // Sorting Middleware
+        $middleware->priority([
+            App\Http\Middleware\EnsureTokenIsValid::class,
+            App\Http\Middleware\First::class,
+            App\Http\Middleware\Second::class,
+        ]);
     })
 
     ->withExceptions(function (Exceptions $exceptions) {
