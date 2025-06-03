ca9c9daa10dd4397dc04858e7d9794f86229b58c
Aulia Atha <auliaatha123@gmail.com>
2025-06-03 21:00:29 +0700

    Global Middleware

--- laravel 12/the_basic/bootstrap/app.php
+++ laravel 12/the_basic/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\EnsureTokenIsValid;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -10,8 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    // Global Middleware
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(EnsureTokenIsValid::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
