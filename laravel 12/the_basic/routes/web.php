be3f130cc2365f4a964467b90fa3ec42b5cf534d
Aulia Atha <auliaatha123@gmail.com>
2025-06-03 21:02:35 +0700

    Asssign Middleware To Routes

--- laravel 12/the_basic/routes/web.php
+++ laravel 12/the_basic/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Middleware\EnsureTokenIsValid;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profile', function() {
+    //../
+})->middleware(EnsureTokenIsValid::class);
