f300e74d704e6002f25bd9a43221702e16f98c37
Aulia Atha <auliaatha123@gmail.com>
2025-06-03 21:17:09 +0700

    Middleware Parameters

--- laravel 12/the_basic/routes/web.php
+++ laravel 12/the_basic/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\EnsureTokenIsValid;
+use App\Http\Middleware\EnsureUserHasRole;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,3 +11,7 @@ Route::get('/', function () {
 Route::get('/profile', function() {
     //../
 })->middleware(EnsureTokenIsValid::class);
+
+Route::put('/post/{id}', function() {
+    //.
+})->middleware(EnsureUserHasRole::class.'editor');
